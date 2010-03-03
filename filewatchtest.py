6fc61f9b354e68bc653b3300cd87d21228b0aa8e
Spencer Russell <spencer.f.russell@gmail.com>
2010-03-02 20:15:41 -0500

    added GPL preambles to source files

--- filewatchtest.py
+++ filewatchtest.py
@@ -1,5 +1,23 @@
 #! /usr/bin/python
 
+# Copyright 2010 Spencer Russell
+# 
+# This file is part of pysched.
+# 
+# pysched is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# pysched is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with pysched.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from pysched import Scheduler
 from filewatcher import FileWatcher
 
